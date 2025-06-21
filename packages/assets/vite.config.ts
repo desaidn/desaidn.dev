24f9c1fcb033a836bcd68055f5227c9099a2ddad
Dhairya Desai <me@desaidn.dev>
2025-06-21 01:15:47 -0400

    Add CSS obfuscation

--- packages/assets/vite.config.ts
+++ packages/assets/vite.config.ts
@@ -1,9 +1,20 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
+import TailwindcssMangle from 'unplugin-tailwindcss-mangle/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import cspPlugin from './cspPlugin';
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), cspPlugin()],
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [
+      tailwindcss(),
+      reactRouter(),
+      tsconfigPaths(),
+      cspPlugin(),
+      ...(isProduction ? [TailwindcssMangle()] : []),
+    ],
+  };
 });
