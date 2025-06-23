0048c0cef1c9ff95a1dcbc53815511f862f3fabc
Dhairya Desai <me@desaidn.dev>
2025-06-23 02:22:37 -0400

    Remove Tailwind CSS mangling

--- packages/assets/vite.config.ts
+++ packages/assets/vite.config.ts
@@ -1,16 +1,9 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
-import TailwindcssMangle from 'unplugin-tailwindcss-mangle/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import cspPlugin from './cspPlugin';
 
-export default defineConfig(({ mode }) => {
-  const plugins = [tailwindcss(), reactRouter(), tsconfigPaths(), cspPlugin()];
-
-  if (mode === 'production') {
-    plugins.push(TailwindcssMangle());
-  }
-
-  return { plugins };
-});
+export default defineConfig(() => ({
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), cspPlugin()],
+}));
