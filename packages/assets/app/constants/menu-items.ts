db7b5fb208b203e718b10e5476c30138094f8b0e
Dhairya Desai <me@desaidn.dev>
2025-07-04 18:00:05 -0400

    Reorder menu items

--- packages/assets/app/constants/menu-items.ts
+++ packages/assets/app/constants/menu-items.ts
@@ -1,5 +1,5 @@
 export default [
   { name: 'experience', path: '/experience', external: false },
-  { name: 'projects', path: '/projects', external: false },
   { name: 'newsletter', path: 'https://desaidn.substack.com/', external: true },
+  { name: 'projects', path: '/projects', external: false },
 ] as const;
