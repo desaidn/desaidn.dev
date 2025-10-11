f5f85bbe74c757512b79aac8a499fec61d55521d
Dhairya Desai <me@desaidn.dev>
2025-10-11 10:47:57 -0400

    Update csp plugin file name

--- packages/assets/vite.config.ts
+++ packages/assets/vite.config.ts
@@ -2,7 +2,7 @@ import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import cspPlugin from './cspPlugin';
+import cspPlugin from './vite-csp-plugin';
 
 export default defineConfig(() => ({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), cspPlugin()],
